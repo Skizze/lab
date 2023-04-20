899c4db72e3ca5252ed6b50d34850b31e9f68c4f
Skizze <bdoggames34@gmail.com>
2023-04-20 13:57:00 -0500

    First file

---
+++ account.py
@@ -0,0 +1,24 @@
+class Account:
+    def __init__(self, name):
+        self.__account_name = name
+        self.__account_balance = 0
+
+    def deposit(self, amount):
+        if amount < 0:
+            return False
+        self.__account_balance += amount
+        return True
+
+    def withdraw(self, amount):
+        if amount < 0:
+            return False
+        elif amount > self.__account_balance:
+            return False
+        self.__account_balance -= amount
+        return True
+
+    def get_balance(self):
+        return self.__account_balance
+
+    def get_name(self):
+        return self.__account_name
